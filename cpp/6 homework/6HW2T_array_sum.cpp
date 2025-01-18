bed08162914770f88be10c0d788596c14f03e734
ilya eis <ilya.safronos@gmail.com>
2025-01-18 10:52:33 +0200

    cpp

--- cpp/6 homework/6HW2T_array_sum.cpp
+++ cpp/6 homework/6HW2T_array_sum.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void printArr(int* arr, int* length) {
+void printArr(int *arr, int *length) {
 	cout << "Array data:" << endl;
 	cout << "Value\tAddress" << endl;
-	for (int* i = arr; i < arr + *length; i++) {
+	for (int *i = arr; i < arr + *length; i++) {
 		cout << *i << "\t" << i << endl;
 	}
 }
 
-int sumOfArray(int* arr, int* length) {
-	int sum = 0;
-	for (int* i = arr; i < arr + *length; i++) {
-		sum += *i;
-	}
-	return sum;
-}
-
 int main() {
-	int* length = new int;
+	int *length = new int;
 	*length = 10;
-	int* arr = new int[*length] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11 };
+	int *arr = new int[*length]{1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
 
 	printArr(arr, length);
 	cout << endl;
 
-	int* sum = new int;
-	*sum = sumOfArray(arr, length);
+	int *sum = new int;
+	*sum = 0;
+	for (int *i = arr; i < arr + *length; i++) {
+		*sum += *i;
+	}
 	cout << "Sum is " << *sum << endl;
 
 	delete[] arr;
--- cpp/6 homework/6HW3T_wrong_array.cpp
+++ cpp/6 homework/6HW3T_wrong_array.cpp
@@ -4,17 +4,37 @@ using namespace std;
 void printArr(int *arr, int *length) {
 	cout << "Array data:" << endl;
 	cout << "Value\tAddress" << endl;
-	for (int* i = arr; i < arr + *length; i++) {
-        cout << *i << "\t" << i << endl;
-    }
+	for (int *i = arr; i < arr + *length; i++) {
+		cout << *i << "\t" << i << endl;
+	}
+}
+
+void get_in_between(int *first, int *second) {
+	cout << "Number(s) in between should be: ";
+	int *i = first;
+	(*i)++;
+	for (; *i < *second; (*i)++) {
+		cout << *i << " ";
+	}
+	cout << endl;
 }
 
 void checkIfCorrect(int *arr, int *length) {
-	for (int* i = (arr + 1); i < arr + *length; i++) {
-		if (*(i)-*(i - 1) != 1) {
+	if (*arr != 1) {
+		cout << "The order is wrong in this array, because first element at address " << arr
+			 << " is not 1!" << endl;
+		int *num = new int;
+		*num = 0;
+		get_in_between(num, arr);
+		delete num;
+		return;
+	}
+	for (int *i = (arr + 1); i < arr + *length; i++) {
+		if (*(i) - *(i - 1) != 1) {
 			cout << "The order is wrong in this array, between value "
-				<< *(i - 1) << " at address " << (i - 1)
-				<< " and value " << *i << " at address " << i << endl;
+				 << *(i - 1) << " at address " << (i - 1)
+				 << " and value " << *i << " at address " << i << endl;
+			get_in_between(i - 1, i);
 			return;
 		}
 	}
@@ -25,19 +45,39 @@ int main() {
 	cout << endl;
 
 	int *length = new int;
-	*length = 10;
-	int *arr = new int[*length] {1, 3, 3, 4, 5, 6, 7, 8, 9, 10};
+	*length = 9;
+	int *arr = new int[*length]{4, 5, 6, 7, 8, 9, 10, 11, 12};
 
 	printArr(arr, length);
 	checkIfCorrect(arr, length);
 
 	delete[] arr;
-	
+
+	cout << endl;
+
+	*length = 2;
+	arr = new int[*length]{1, 5};
+
+	printArr(arr, length);
+	checkIfCorrect(arr, length);
+
+	delete[] arr;
+
+	cout << endl;
+
+	*length = 4;
+	arr = new int[*length]{2, 3, 4, 5};
+
+	printArr(arr, length);
+	checkIfCorrect(arr, length);
+
+	delete[] arr;
+
 	cout << endl;
 
 	*length = 5;
-	arr = new int[*length] {1, 2, 3, 4, 5};
-	
+	arr = new int[*length]{1, 2, 3, 4, 5};
+
 	printArr(arr, length);
 	checkIfCorrect(arr, length);
 
